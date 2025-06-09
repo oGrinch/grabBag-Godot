2f32330994e698c11b54037d6c6b07d1dd15752d
_grinch <129088738+oGrinch@users.noreply.github.com>
2025-06-09 04:26:24 -0300

    Adicionada versão compacta do código.

---
+++ projetos/movimento/basico/Scripts/Player-compact.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 500.0
+const FRICTION = 10000.0
+
+func _physics_process(delta: float) -> void:
+	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if input.length() > 0:
+		velocity = input * MAX_SPEED
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+	move_and_slide()
